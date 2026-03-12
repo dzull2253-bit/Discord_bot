fd562fb7e853c97ee61c19e17d9911d289fe7ac0
dzull2253-bit <dzull2253@gmail.com>
2026-03-12 20:36:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on('ready', () => {
+  console.log('Bot sudah online');
+});
+
+client.on('messageCreate', message => {
+  if(message.content === '!ping'){
+    message.reply('pong');
+  }
+});
+
+client.login(process.env.TOKEN);
